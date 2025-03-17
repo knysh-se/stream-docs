375a466d6b2cb701be2d11f9dd26c8147b50c501
Serhii Knysh <knysh.se@gmail.com>
2025-03-17 10:48:54 +0200

    initial commit

---
+++ zudoku.config.mjs
@@ -0,0 +1,32 @@
+/** @type {import('zudoku').ZudokuConfig} */
+const config = {
+    basePath: "/learning-openapi-gen/streaming-api-documentation/",
+    topNavigation: [
+        {id: "docs", label: "Documentation"},
+        {id: "api", label: "API Reference"},
+        {id: "about", label: "About"}
+    ],
+    sidebar: {
+        docs: [
+            {
+                type: "category",
+                label: "Documentation",
+                items: ["/docs/installation", "/docs/authorization", "/docs/about"]
+            }
+        ]
+    },
+    redirects: [
+        {from: "/", to: "/docs/installation"},
+        { from: "/about", to: "/docs/about" }
+    ],
+    apis: {
+        type: "file",
+        input: "./apis/openapi.yaml",
+        navigationId: "api"
+    },
+    docs: {
+        files: "/pages/**/*.{md,mdx}",
+    }
+};
+
+export default config;
